187a2e1280ec8e3dddd36ff31658097d892e82d5
Sabyoud Zohair <109246617+ZouhairSA@users.noreply.github.com>
2025-04-23 02:56:51 +0100

    Ajout V1

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/AI_SECURITY/',
   server: {
     host: "::",
     port: 8080,
